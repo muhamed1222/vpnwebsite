654a8a543e38bfd112a71b31afa78c5f3dbb802d
outcastsdev-svg <outcastsdev@gmail.com>
2026-01-07 01:51:52 +0300

    ui: update background circles dimensions for improved layout

--- components/ui/BackgroundCircles.tsx
+++ components/ui/BackgroundCircles.tsx
@@ -10,7 +10,7 @@ interface BackgroundCirclesProps {
  */
 export const BackgroundCircles = memo<BackgroundCirclesProps>(({ children }) => {
   return (
-    <div className="absolute top-0 left-0 right-0 h-[560px] flex items-end justify-center opacity-100 pointer-events-none">
+    <div className="absolute top-0 left-0 right-0 h-[420px] flex items-end justify-center opacity-100 pointer-events-none">
       <div className="absolute w-[560px] h-[560px] border border-white/40 rounded-full flex flex-col items-center justify-center">
         <div className="relative w-32 h-32 flex items-center justify-center">
           {children}
@@ -18,7 +18,7 @@ export const BackgroundCircles = memo<BackgroundCirclesProps>(({ children }) => 
       </div>
       <div className="absolute w-[450px] h-[450px] border border-white/30 rounded-full" />
       <div className="absolute w-[600px] h-[600px] border border-white/20 rounded-full opacity-80" />
-      <div className="absolute w-[360px] h-[360px] border border-white/10 rounded-full" />
+      <div className="absolute w-[360px] h-[360px] border border-white/10 rounded-full flex flex-col items-center justify-center" />
     </div>
   );
 });
